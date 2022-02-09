70f07c713b39bf52925888fab185b3ca4cc4088b
MorGuux <morgan.gardner2112@gmail.com>
2022-02-09 17:26:20 +0000

    Bilinear interpolation fully functional.

--- src/test.java
+++ src/test.java
@@ -41,6 +41,14 @@ public class test extends Application {
     float[][][] grey; //store the 3D volume data set converted to 0-1 ready to copy to the image
     short min, max; //min/max value in the 3D volume data set
     ImageView TopView;
+    int imageSize = 256; //size of the image
+
+    enum ImageInterpolation {
+        NEAREST_NEIGHBOUR,
+        BILINEAR
+    }
+
+    ImageInterpolation activeInterpolation = ImageInterpolation.NEAREST_NEIGHBOUR;
 
     public static void main(String[] args) {
         launch();
@@ -62,7 +70,7 @@ public class test extends Application {
         //int width=1024, height=1024; //maximum size of the image
         //We need 3 things to see an image
         //1. We need to create the image
-        Image top_image = GetSlice(76); //go get the slice image
+        Image top_image = GetSlice(76, 256, ImageInterpolation.NEAREST_NEIGHBOUR); //go get the slice image
         //2. We create a view of that image
         TopView = new ImageView(top_image); //and then see 3. below
 
@@ -86,9 +94,13 @@ public class test extends Application {
 
                 if (rbNN.isSelected()) {
                     System.out.println("Nearest Neighbour selected");
+                    activeInterpolation = ImageInterpolation.NEAREST_NEIGHBOUR;
+                    updateImage();
                 }
                 else if (rbB.isSelected()) {
                     System.out.println("Bilinear selected");
+                    activeInterpolation = ImageInterpolation.BILINEAR;
+                    updateImage();
                 }
             }
         });
@@ -98,11 +110,9 @@ public class test extends Application {
             public void changed(ObservableValue<? extends Number>
                                         observable, Number oldValue, Number newValue) {
 
-                System.out.println(newValue.intValue());
-                //Here's the basic code you need to update an image
-                TopView.setImage(null); //clear the old image
-                Image newImage = GetSlice(76); //go get the slice image
-                TopView.setImage(newImage); //Update the GUI so the new image is displayed
+                imageSize = newValue.intValue();
+                System.out.println(imageSize);
+                updateImage();
             }
         });
 
@@ -128,6 +138,12 @@ public class test extends Application {
         stage.show();
 
         ThumbWindow(scene.getX() + 200, scene.getY() + 200);
+    }
+
+    private void updateImage() {
+        TopView.setImage(null); //clear the old image
+        Image newImage = GetSlice(76, imageSize, activeInterpolation); //go get the slice image
+        TopView.setImage(newImage); //Update the GUI so the new image is displayed
     }
 
     //Function to read in the cthead data set
@@ -183,8 +199,8 @@ public class test extends Application {
     }
 
     //Gets an image from slice at given index
-    public Image GetSlice(int sliceIndex) {
-        WritableImage image = new WritableImage(256, 256);
+    public Image GetSlice(int sliceIndex, int size, ImageInterpolation interpolation) {
+        WritableImage image = new WritableImage(size, size);
         //Find the width and height of the image to be process
         int width = (int) image.getWidth();
         int height = (int) image.getHeight();
@@ -194,17 +210,97 @@ public class test extends Application {
         PixelWriter image_writer = image.getPixelWriter();
 
         //Iterate over all pixels
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                //For each pixel, get the colour from the cthead slice index
-                val = grey[sliceIndex][y][x];
-                Color color = Color.color(val, val, val);
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+
+                //Get resized image pixel value
+                float y = (float) j * 256 / height;
+                float x = (float) i * 256 / width;
+
+                if (interpolation == ImageInterpolation.NEAREST_NEIGHBOUR) {
+                    //Nearest neighbour interpolation
+                    val = grey[sliceIndex][(int) y][(int) x];
+                }
+                else {
+                    //Bilinear interpolation
+                    float[] quadPixels = getQuadPixelsColours(x, y, sliceIndex);
+
+                    float newX = (float) (x - 0.5);
+                    float a1 = newX - (int) newX;
+                    float newY = (float) (y - 0.5);
+                    float a2 = newY - (int) newY;
+
+                    val = bilinearInterpolate(
+                            quadPixels[0],
+                            quadPixels[1],
+                            quadPixels[2],
+                            quadPixels[3],
+                            a1, a2
+                    );
+                }
 
                 //Apply the new colour
-                image_writer.setColor(x, y, color);
+                image_writer.setColor(i, j, Color.color(val, val, val));
             }
         }
+
         return image;
+    }
+
+    /**
+     * Gets the colour of the four pixels that are closest to the given x and y coordinates
+     *
+     * @param x          The x coordinate
+     * @param y          The y coordinate
+     * @param sliceIndex The slice index
+     * @return An array of the four colours of the four pixels that are closest to the given x and y coordinates
+     */
+    private float[] getQuadPixelsColours(float x, float y, int sliceIndex) {
+        // slice, Y, X
+        if (x + 0.5 >= 256) {
+            x = 255;
+        }
+        if (y + 0.5 >= 256) {
+            y = 255;
+        }
+        float X1 = grey[sliceIndex][(int) (y - 0.5)][(int) (x - 0.5)];
+        float X2 = grey[sliceIndex][(int) (y - 0.5)][(int) (x + 0.5)];
+        float Y1 = grey[sliceIndex][(int) (y + 0.5)][(int) (x - 0.5)];
+        float Y2 = grey[sliceIndex][(int) (y + 0.5)][(int) (x + 0.5)];
+        return new float[]{X1, X2, Y1, Y2};
+    }
+
+    /**
+     * Lerp between two values
+     *
+     * @param x1 first value
+     * @param x2 second value
+     * @param x  interpolation value
+     * @return lerped value
+     */
+    public float lerp(float x1, float x2, float x) {
+        return x1 * (1 - x) + x2 * x;
+    }
+
+    /**
+     * Bilinear interpolation
+     *
+     * @param x1 first x value
+     * @param x2 second x value
+     * @param y1 first y value
+     * @param y2 second y value
+     * @param x  x interpolation value
+     * @param y  y interpolation value
+     * @return bilinear interpolated value
+     */
+    public float bilinearInterpolate(float x1,
+                                     float x2,
+                                     float y1,
+                                     float y2,
+                                     float x, float y) {
+        float xLerp = lerp(x1, x2, x);
+        float yLerp = lerp(y1, y2, x);
+        return lerp(xLerp, yLerp, y);
     }
 
     public void ThumbWindow(double atX, double atY) {
